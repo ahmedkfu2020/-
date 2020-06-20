ab541c9a197a6890fc922e8fdae34371cf306307
ahmedkfu2020 <66139259+ahmedkfu2020@users.noreply.github.com>
2020-06-21 00:37:45 +0300

    Create ahmed25_1.py

---
+++ ahmed25_1.py
@@ -0,0 +1,9 @@
+from math import pi, sin, cos
+
+theta1 = pi/4
+theta2 = pi/6
+
+print(round(cos(theta1),3),-round(sin(theta1),3),0,0)
+print(round(sin(theta1),3),-round(cos(theta1),3),0,0)
+print(0,0,round(cos(theta2),3),-round(sin(theta2),3))
+print(0,0,round(sin(theta2),3),-round(cos(theta2),3))
